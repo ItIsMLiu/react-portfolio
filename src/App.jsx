92ea19ebaee08790bfb3a27c4cc17b500aca5c5c
ItIsMLiu <mliu.design@outlook.com>
2024-01-28 23:28:28 +0000

    App.jsx imported router & component routes added in function.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Contact from "./components/Contact.jsx"
 import Header from "./components/Header.jsx"
 import Home from "./components/Home.jsx"
 import Project from "./components/Project.jsx"
 import ProjectGallery from "./components/ProjectGallery.jsx"
 import Footer from "./components/Footer.jsx"
+
 import { useState } from 'react'
 import './App.css'
 import "./style/Global.css"
@@ -12,14 +15,16 @@ import "./style/Global.css"
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-      <Project />
-      <ProjectGallery />
-      <Contact />
-      <Footer />
-    </>
+  <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<Project />} />
+      <Route path="blog" element={<ProjectGallery />} />
+      <Route path="contact/*" element={<Contact />} />
+    </Routes>
+    <Footer />
+  </Router>
   )
 }
 
