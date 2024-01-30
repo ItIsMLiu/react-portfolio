f0e8e50ba45a89958d4b172c90f7c0acdd044303
ItIsMLiu <mliu.design@outlook.com>
2024-01-30 14:43:19 +0000

    project component tidied up and link hover effect changed.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,24 +4,22 @@ import "../style/Project.css"
 
 function Project(props) {
   return <>
-    
-      <div className="card col-lg-6 col-md-6 col-sm-12 img-link" id="project">
-        <img src={ props.image } className="card-img" alt={`${ props.title } project image`}></img>
-        <div className="card-img-overlay">
-          <div className="subheading">
-            <h5 className="card-title"><strong>Project Title:</strong> { props.title }</h5>
-            <p className="card-text">
-              <strong>Deployed App:</strong>
-              <a href={`${ props.deployed }`} class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> {props.deployed}</a>
-            </p>
-            <p className="card-text">
-              <strong>Repo Link:</strong>
-              <a href={`${ props.repo }`} class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> {props.repo}</a>
-            </p>
-          </div>
+    <div className="card col-lg-6 col-md-6 col-sm-12 img-link" id="project">
+      <img src={ props.image } className="card-img" alt={`${ props.title } project image`}></img>
+      <div className="card-img-overlay">
+        <div className="subheading">
+          <h5 className="card-title"><strong>Project Title:</strong> { props.title }</h5>
+          <p className="card-text">
+            <strong>Deployed App:</strong>
+            <a href={`${ props.deployed }`} class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> {props.deployed}</a>
+          </p>
+          <p className="card-text">
+            <strong>Repo Link:</strong>
+            <a href={`${ props.repo }`} class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"> {props.repo}</a>
+          </p>
         </div>
       </div>
-      
+    </div>
   </>
 }
 
