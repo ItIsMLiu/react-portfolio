c2cce92a1fd912bf32bba0a5fed95c01b340e8f8
ItIsMLiu <mliu.design@outlook.com>
2024-01-30 15:49:00 +0000

    project component image link changed.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,9 +3,10 @@ import "../style/Global.css"
 import "../style/Project.css"
 
 function Project(props) {
+  const baseUrl = process.env.PUBLIC_URL || "";
   return <>
     <div className="card col-lg-6 col-md-6 col-sm-12 img-link" id="project">
-      <img src={ props.image } className="card-img" alt={`${ props.title } project image`}></img>
+      <img src={`${baseUrl}/${props.image}`} className="card-img" alt={`${ props.title } project image`}></img>
       <div className="card-img-overlay">
         <div className="subheading">
           <h5 className="card-title"><strong>Project Title:</strong> { props.title }</h5>
